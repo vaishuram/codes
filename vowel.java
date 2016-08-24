cfc0cd177fef06dc55cb95c7372a62abc2ab0810
vaishnavi <vaishnavi.dain@gmail.com>
2016-08-24 14:59:03 +0530

    Create vowel.java

---
+++ vowel.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+public class vowel{
+public static void main(String[] args) throws Exception 
+{
+char n;
+BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+System.out.print("Enter the character you want to check:");
+n = (char) bf.read();
+switch(n)
+{
+case 'a':
+System.out.print("The given character "+n+" is vowel");
+break;
+case 'e':
+System.out.print("The given character "+n+" is vowel");
+break;
+case 'i':
+System.out.print("The given character "+n+" is vowel");
+break;
+case 'o':
+System.out.print("The given character "+n+" is vowel");
+break;
+case 'u':
+System.out.print("The given character "+n+" is vowel");
+break;
+default:
+System.out.println("The given character "+n+" is consonant");
+break;}}}
+
