d764dc9994f426e14d7b09916d095b726a8c835e
vaishnavi <vaishnavi.dain@gmail.com>
2016-08-24 15:33:59 +0530

    Create sumnnum.java

---
+++ sumnnum.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+class sumnnum
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+int num=sc.nextInt();
+int sum=0;
+if(num>0)
+{
+for(int i=0;i<=num;i++)
+{
+sum=sum+i;
+}
+}
+System.out.print(" " +sum);
+}
+}
