a29632fd028da01b06f29ad89ba1aef7eaf9f170
vaishnavi <vaishnavi.dain@gmail.com>
2016-08-24 16:08:25 +0530

    Create evennum.java

---
+++ evennum.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class evennum{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+int num=sc.nextInt();
+int num2=sc.nextInt();
+for(int a=num;i<=num2;i++)
+{
+if((a%2)==0)
+System.out.println(" "+a);
+}
+}
+}
