e1edd1abdfb462c253db9ffe3d8f97aa6cd102dc
vaishnavi <vaishnavi.dain@gmail.com>
2016-08-24 15:24:35 +0530

    Create hello.java

---
+++ hello.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class hello
+{
+public static void main(String args[])
+{
+Scanner s=new Scanner(System.in);
+String str=sc.nextLine();
+int n=0;
+for(;n<5;n++)
+{
+System.out.println(" " +a);
+}
+}
+}
