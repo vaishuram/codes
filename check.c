a45d78e3544d6a87606cc33810507e05f90d5183
vaishnavi <vaishnavi.dain@gmail.com>
2016-08-24 14:51:14 +0530

    Create check.c

---
+++ check.c
@@ -0,0 +1,14 @@
+
+#include<stdio.h>
+#include<conio.h>
+ void main()
+{
+int a;
+scanf("%d",&a);
+if(a>0)
+System.out.println("positive");
+elseif(a<0)
+System.out.println("negative");
+else
+System.out.println("zero");
+}
