a82b379d290e31ca5a3506fc47d9a73d2a600457
vaishnavi <vaishnavi.dain@gmail.com>
2016-08-24 14:40:29 +0530

    Create check1.java

---
+++ check1.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class check1{
+public static void main(String args[])
+{
+Scanner s=new Scanner(System.in);
+int a=s.nextInt();
+if(a>0)
+System.out.println("positive");
+elseif(a<0)
+System.out.println("negative");
+else
+System.out.println("zero");
+}
+}
