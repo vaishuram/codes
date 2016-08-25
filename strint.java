5086361e32c4869414f6d1dc12c5ce40cc6ba5da
vaishnavi <vaishnavi.dain@gmail.com>
2016-08-25 10:14:13 +0530

    Create strint.java

---
+++ strint.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+import java.util.lang;
+class strint 
+{
+public static void main(String[] args) 
+{
+Scanner s=new Scanner(System.in);
+String num = s.nextLine();
+int aaa=Integer.parseInt(num.trim());
+System.out.println(""+aaa);
+}
+}
