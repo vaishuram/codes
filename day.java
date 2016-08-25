eb0fbe0e9a6cac650f55a724f421381b455ac93e
vaishnavi <vaishnavi.dain@gmail.com>
2016-08-25 10:49:03 +0530

    Create day.java

---
+++ day.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class day{
+public static void main(String args[]){
+Scanner sc=new Scanner(System.in);
+string s=sc.nextLine();
+if(s=="sunday")
+{
+	System.out.println(s+" is a holiday");
+}
+else
+{System.out.println(s+" is  not a holiday");
+}
+}
+}
