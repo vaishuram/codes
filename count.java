58063611c3abf3bf02b5c75dc5f548e7be211fe0
vaishnavi <vaishnavi.dain@gmail.com>
2016-08-25 11:51:58 +0530

    Create count.java

---
+++ count.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class count{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+String name="javatpoint";
+int i=0,count=0;
+if(name.charAt(i)!="\0")
+{count=count+1;
+i++;
+}System.out.println(count);
+}}
