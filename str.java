6884b7c3428ebea957777af5a0e9693a2aad2f6d
vaishnavi <vaishnavi.dain@gmail.com>
2016-08-25 11:02:26 +0530

    Create str.java

---
+++ str.java
@@ -0,0 +1,9 @@
+import java.io.*;
+public class str{
+ public static void main(String args[]){
+ String Str = new String("java");
+System.out.print("Return Value :" );
+System.out.println(Str.replace('j', 'i'));
+System.out.println(Str.replace('a', 'z'));
+System.out.println(Str.replace('v', 'u'));
+}
