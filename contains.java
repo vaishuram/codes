9c90e91ca12f9564e90561081dd1baa5be682d13
vaishnavi <vaishnavi.dain@gmail.com>
2016-08-26 11:33:55 +0530

    Create contains.java

---
+++ contains.java
@@ -0,0 +1,10 @@
+
+import java.util.*;
+class contains{  
+public static void main(String args[]){  
+String name="what do you know about me";  
+String name1="vaishu";  
+System.out.println(name.contains("do you know"));  
+System.out.println(name.contains("about"));  
+System.out.println(name1.contains("vaishu"));  
+}}  
