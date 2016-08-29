8721f8a9a89514930a9df283747fa48ae3be91d5
vaishnavi <vaishnavi.dain@gmail.com>
2016-08-29 16:32:15 +0530

    Create ocur.java

---
+++ ocur.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+class ocur
+{
+public static void main(String args[])
+{
+Scanner sn=new Scanner(System.in);
+		System.out.println("enter the number");
+		int count=0;
+		int num=sn.nextInt();
+		int a[]=new int[100];
+		int check=7;
+		for(int i=0;i<num;i++)
+		{
+			int b=num%10;
+			num=num/10;
+		     a[i]=b;
+		     if(a[i]==check)
+		     {
+		    	 count++;
+		     }
+		     }System.out.println(count);
+		     }
+		     }
