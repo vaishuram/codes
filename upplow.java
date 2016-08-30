55b937d71f772f73c2da633dd9b04abb3215736e
vaishnavi <vaishnavi.dain@gmail.com>
2016-08-30 15:03:09 +0530

    Create upplow.java

---
+++ upplow.java
@@ -0,0 +1,20 @@
+import java.util.*;
+class upplow
+{
+public static void main(String args[])
+{
+String u="Welcome";
+char s[]=s.toCharArray();
+for(int i=0;i<s.length;i++)
+{
+if(Character.isUpperCase(s[i]))
+{
+System.out.print(Character.isLowerCase(s[i]));
+}
+if(Character.isLowerCase(s[i]))
+{
+System.out.print(Character.isUpperCase(s[i]));
+}
+}
+}
+}
