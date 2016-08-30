f85a7458538296a53a0d0cfa689e662e27d702c8
vaishnavi <vaishnavi.dain@gmail.com>
2016-08-30 14:52:45 +0530

    Create swapp.java

---
+++ swapp.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+class swapp
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+int a=sc.nextInt();
+int b=sc.nextInt();
+int t;
+t=a;
+a=b;
+b=t;
+system.out.print(""+a);
+System.out.print(""+b);
+}}
