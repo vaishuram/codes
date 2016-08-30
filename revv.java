6d2d125b14e11333f5b7ad5d891369cb2f1b36cb
vaishnavi <vaishnavi.dain@gmail.com>
2016-08-30 14:22:49 +0530

    Create revv.java

---
+++ revv.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.util.Scanner;
+class revv
+{
+Public static void main(String str)
+{
+Scanner sc=new Scanner(System.in);
+String s=sc.newLine();
+String st=s.reverse();
+System.out.print(st);
+}
